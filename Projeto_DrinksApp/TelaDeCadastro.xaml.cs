f882623e8a1b359a445aabda99d8c16103c44c12
davineves008 <Davineves418@gmail.com>
2026-03-06 21:30:49 -0300

    mudanca de cor e melhoria nas bordas

--- Projeto_DrinksApp/TelaDeCadastro.xaml.cs
+++ Projeto_DrinksApp/TelaDeCadastro.xaml.cs
@@ -27,13 +27,62 @@ namespace Projeto_DrinksApp
 
         private void BtnCadastrar_Click(object sender, RoutedEventArgs e)
         {
-            // Capturando os dados das TextBoxes que espalhamos na tela
+            // Capturando os dados das TextBoxes 
             string nome = txtNome.Text;
             string email = txtEmail.Text;
             string cidade = txtCidade.Text;
             string cpf = txtCPF.Text;
             string senha = txtSenha.Password; // PasswordBox usa .Password em vez de .Text
             string usuario = txtUsuario.Text; // Novo campo
+
+            // VALIDAÇÕES
+            //val. nome
+            if (string.IsNullOrWhiteSpace(nome))
+            {
+                MessageBox.Show("Digite o nome.");
+                txtNome.Focus();
+                return;
+            }
+
+           
+            //valida usuario;
+            if (string.IsNullOrWhiteSpace(usuario))
+            {
+                MessageBox.Show("Digite o usuário.");
+                txtUsuario.Focus();
+                return;
+            }
+
+            //val. senha;
+            if (string.IsNullOrWhiteSpace(senha))
+            {
+                MessageBox.Show("Digite a senha.");
+                txtSenha.Focus();
+                return;
+            }
+            //val. senha;
+            if (!cpf.All(char.IsDigit))
+            {
+                MessageBox.Show("CPF deve conter apenas números.");
+                txtCPF.Focus();
+                return;
+            }
+
+            //val. email;
+            if (!email.Contains("@") || !email.Contains("."))
+            {
+                MessageBox.Show("Email inválido.");
+                txtEmail.Focus();
+                return;
+            }
+
+            //val. senha;
+            if (senha.Length < 3)
+            {
+                MessageBox.Show("A senha deve ter pelo menos 3 caracteres.");
+                txtSenha.Focus();
+                return;
+            }
 
             ClienteRepositorio repo = new ClienteRepositorio();
 
@@ -52,6 +101,11 @@ namespace Projeto_DrinksApp
         private void BtnVoltar_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void txtNome_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
